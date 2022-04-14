c7104ca1bd8167cf1960e14ee717ee81f6c1e51e
Demmy <demolaabdulai@live.com>
2022-04-14 13:43:37 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string | undefined;
+  date: string | undefined;
+  amount: number | undefined;
+  height: number | undefined;
+  miles: number | undefined; 
+
+
+  onNameChange(value: string) {
+    this.name = value;
+
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value:string){
+    this.height =parseFloat(value);
+  }
+
+  onMilesChange(value:number){
+    this.miles = value
+  }
+}
